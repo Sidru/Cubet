914a75a58b1aba5a271f7ed030414f34028050c5
Sidru <nagemm@gmail.com>
2020-02-01 21:52:13 +0100

    First hardware tests...

---
+++ Software/include/configuration.h
@@ -0,0 +1,7 @@
+#define DEBUG_LED_PIN   19  // PIN_PB02 -> 19 for Arduino
+#define BUTTON_PIN      2   // PIN_PA14 -> 2 for Arduino
+#define SHIFT_DATA_PIN  11  // PIN_PA16 -> 11 for Arduino
+#define SHIFT_CLK_PIN   13  // PIN_PA17 -> 13 for Arduino
+#define SHIFT_LATCH_PIN 10  // PIN_PA18 -> 10 for Arduino
+
+#define SHIFT_READ_PIN  9   // PIN_PA07 -> 9 for Arduino. Not used, only to test the Shifty library
--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -5,25 +5,82 @@
  */
 
 #include <Arduino.h>
+#include <ArduinoLog.h>
+#include <Shifty.h>
 
-#define DEBUG_PIN 19 
-//#define DEBUG_PIN PIN_PB02
-//
-void setup()
+#include <configuration.h>
+
+#define SHIFTY_NR_BITS 72
+Shifty shift;
+
+void shift_init(void);
+static void toggle_led_debug(void);
+static void shifty_test(void);
+
+void shift_init(void)
 {
-  //  while(!Serial);
-  // initialize LED digital pin as an output.
-  pinMode(DEBUG_PIN, OUTPUT);
+    shift.setBitCount(SHIFTY_NR_BITS); // 8x8 Columns and 8 Layers
+    shift.setPins(SHIFT_CLK_PIN, SHIFT_DATA_PIN, SHIFT_LATCH_PIN, SHIFT_READ_PIN);
 }
 
+void toggle_led_debug(void) {
+    static bool last = 0;
+    static unsigned long start_millis = millis();
+    
+    if (digitalRead(BUTTON_PIN) == (bool)0) // Test the Board Button
+    {
+        if ((millis() - start_millis) >= 250)
+        {
+            start_millis = millis();
+            last = !last;
+            digitalWrite(DEBUG_LED_PIN, (bool)last);
+        }
+
+    }
+    else if ((millis() - start_millis) >= 1000)
+    {
+        start_millis = millis();
+        last = !last;
+        digitalWrite(DEBUG_LED_PIN, (bool)last);
+    }
+}
+
+void shifty_test(void)
+{
+    static unsigned long start_millis = millis();
+    static uint32_t cnt = 0; 
+    uint32_t i = 0;
+
+    if ((millis() - start_millis) >= 100)
+    {
+        start_millis = millis();
+
+        if (cnt >= SHIFTY_NR_BITS)
+        {
+            cnt = 0;
+        }
+        shift.batchWriteBegin();
+        for(i = 0; i <= SHIFTY_NR_BITS; i++)
+        {
+            shift.writeBit(i, 0);
+        }
+        shift.writeBit(cnt, 1);
+        shift.batchWriteEnd();
+        cnt++;
+    }
+}
+void setup()
+{
+  // initialize LED digital pin as an output.
+    pinMode(DEBUG_LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT);
+    Serial.begin(115200);
+    Serial.println("Hello World");
+    Log.begin(LOG_LEVEL_VERBOSE, &Serial);
+    shift_init();
+}
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(DEBUG_PIN, HIGH);
-  // wait for a second
-  delay(1000);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(DEBUG_PIN, LOW);
-   // wait for a second
-  delay(1000);
+    toggle_led_debug();
+    shifty_test();
 }
